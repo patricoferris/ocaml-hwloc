eb3d8401930c080fb71710b1a615a4acc84761ba
benmandrew <benmandrew@gmail.com>
2023-11-08 14:31:50 +0100

    format ocaml

--- src/c/function_description.ml
+++ src/c/function_description.ml
@@ -8,14 +8,18 @@ module Functions (F : Ctypes.FOREIGN) = struct
     let init = foreign "hwloc_topology_init" (ptr T.Topology.t @-> returning int)
     let load = foreign "hwloc_topology_load" (T.Topology.t @-> returning int)
 
-    let destroy = foreign "hwloc_topology_destroy" (T.Topology.t @-> returning void)
+    let destroy =
+      foreign "hwloc_topology_destroy" (T.Topology.t @-> returning void)
 
-    let depth = foreign "hwloc_topology_get_depth" (T.Topology.t @-> returning int)
+    let depth =
+      foreign "hwloc_topology_get_depth" (T.Topology.t @-> returning int)
   end
 
   module Object = struct
     module Type = struct
-      let get_type_depth = foreign "hwloc_get_type_depth" (T.Topology.t @-> T.Object.Type.t @-> returning int)
+      let get_type_depth =
+        foreign "hwloc_get_type_depth"
+          (T.Topology.t @-> T.Object.Type.t @-> returning int)
     end
   end
 end
--- src/lib/hwloc.ml
+++ src/lib/hwloc.ml
@@ -2,27 +2,27 @@ open Ctypes
 module T = Hwloc_c.C.Type
 module F = Hwloc_c.C.Functions
 
-let check_err = function
-  | 0 -> ()
-  | n -> failwith (string_of_int n)
-
-
+let check_err = function 0 -> () | n -> failwith (string_of_int n)
 
 module Topology = struct
   type t = T.Topology.t Ctypes_static.structure ptr ptr
 
-  let create () = 
-    let ptr = allocate ~finalise:(fun ptr -> F.Topology.destroy (!@ ptr)) T.Topology.t (Ctypes.from_voidp T.Topology.hwloc null) in
+  let create () =
+    let ptr =
+      allocate
+        ~finalise:(fun ptr -> F.Topology.destroy !@ptr)
+        T.Topology.t
+        (Ctypes.from_voidp T.Topology.hwloc null)
+    in
     check_err @@ F.Topology.init ptr;
     ptr
 
-  let load t = check_err @@ F.Topology.load (!@ t)
-  let depth t = F.Topology.depth (!@ t)
-
+  let load t = check_err @@ F.Topology.load !@t
+  let depth t = F.Topology.depth !@t
 end
 
 module Object = struct
   module Type = struct
-    let get_type_depth top = F.Object.Type.get_type_depth (!@ top)
+    let get_type_depth top = F.Object.Type.get_type_depth !@top
   end
 end
